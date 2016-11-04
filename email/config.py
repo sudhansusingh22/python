4faa3579fa92e8d335c73e783ab9588e0f3d4f31
sudhansusingh22 <sudhansusingh22@gmail.com>
2016-11-04 16:03:43 -0400

    removed unwanted file

--- email/config.py
+++ email/config.py
@@ -5,14 +5,22 @@
 # modifiy according to your requirement
 
 config = {}
-config['senderEmail'] = "---your email---"
-config['password'] = "---your email password--"
-config['filename'] = "--filename you want to attach --"
-config['attachment'] = "--attachment name (can be same as filename)--"
-config['subject'] = " -- subject of email (like Python is Great!!)---- "
-config['body'] = """---body of your email -----\n"""
-config['signature'] = """--your signature ---- """
-# this is the csv file where you can store the information of recipients
+config['senderEmail'] = "ssingh25@ncsu.edu"
+config['password'] = "triples1990"
+config['filename'] = "Sudhansu_Resume_SE.pdf"
+config['attachment'] = "Sudhansu_Resume_SE.pdf"
+config['subject'] = "Looking for Full-Time Opportunity in Software Development/Engineering at "
+config['body'] = """I am Sudhansu, a Master's student in Computer Science, at North Carolina State University. 
+I have prior experience of 4 years at Infosys Ltd. I will be graduating in May, 2017 and, currently looking for full time opportunities in Software Engineering/Development field.\n
+Please find my resume attached to this mail and feel free to reach me for any additional information.
+Looking forward to hearing from you.\n"""
+config['signature'] = """-- 
+Yours sincerely,
+Sudhansu Singh
++1-984-202-9151
+Graduate Student
+Department of Computer Science
+North Carolina State University """
 config['emailFile'] = "test.csv"
 
 #print(config)
